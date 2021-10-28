9aa975ce285e5412415150643532c157ffc8bd85
Matthew Ashley <matthewashley1@gmail.com>
2021-10-28 16:25:16 -0400

    Updated the KaruraClient class
    Added a transfer function to the KaruraClient class and added function comments.
    - ./connectors/karura/karura-client.ts

--- build/connectors/karura/karura-client.js
+++ build/connectors/karura/karura-client.js
@@ -11,12 +11,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const api_1 = require("@acala-network/api");
 const api_2 = require("@polkadot/api");
+/**
+ * KaruraClient connects to the Polkadot.js API
+ */
 class KaruraClient {
+    /**
+     *
+     * @param {string}      address  Karura account address
+     * @param {Object}      [logger=console]    A logger function, (default is console logging)
+     */
     constructor(address, logger = console) {
         this.address = address;
         this.logger = logger;
         this.provider = new api_2.WsProvider("wss://karura.api.onfinality.io/public-ws");
     }
+    /**
+     * Retrieves account data for the Karura account address of the class
+     *
+     * @returns {Promise<FrameSystemAccountInfo>}
+     */
     accountData() {
         return __awaiter(this, void 0, void 0, function* () {
             const provider = new api_2.WsProvider("wss://karura.api.onfinality.io/public-ws");
@@ -26,13 +39,19 @@ class KaruraClient {
             return accountData.toHuman();
         });
     }
-    getFees(recipient, sender) {
+    /**
+     * Gets potential fees for a transaction on Karura
+     *
+     * @param {string}      recipient   transaction receiver address
+     * @returns {Promise<RuntimeDispatchInfo>}
+     */
+    getFees(recipient) {
         return __awaiter(this, void 0, void 0, function* () {
             const api = new api_2.ApiPromise((0, api_1.options)({ provider: this.provider }));
             yield api.isReadyOrError;
             const info = yield api.tx.balances
                 .transfer(recipient, 123)
-                .paymentInfo(sender);
+                .paymentInfo(this.address);
             const [amount, unit] = info.partialFee.toHuman().split(" ");
             return `${this.karUnitsToKAR(amount, unit)} KAR`;
         });
@@ -50,6 +69,24 @@ class KaruraClient {
         else if (unit === "KAR") {
             return amount;
         }
+    }
+    /**
+     * Transfers a token from the class address to a recipient Karura account address
+     *
+     * @param {string}      recipient   transfer receiver address
+     * @param {string}      token   id of token to transfer - Examples "KSM", "KAR"
+     * @param {string}      amount  amount of tokens to transfer
+     */
+    transfer(recipient, token, amount) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const api = new api_2.ApiPromise((0, api_1.options)({ provider: this.provider }));
+            yield api.isReadyOrError;
+            this.accountData().then((data) => this.logger.log("Before transfer account data", data));
+            const hash = yield api.tx.currencies
+                .transfer(recipient, { Token: token, }, amount)
+                .signAndSend(this.address);
+            this.logger.log("Transfer sent with hash", hash.toHex());
+        });
     }
 }
 module.exports = KaruraClient;
--- connectors/karura/karura-client.ts
+++ connectors/karura/karura-client.ts
@@ -7,14 +7,27 @@ interface KaruraClient {
     provider: WsProvider
 }
 
+/**
+ * KaruraClient connects to the Polkadot.js API 
+ */
 class KaruraClient {
 
+    /**
+     * 
+     * @param {string}      address  Karura account address
+     * @param {Object}      [logger=console]    A logger function, (default is console logging) 
+     */
     constructor(address: string, logger: Console = console) {
         this.address = address;
         this.logger = logger;
         this.provider = new WsProvider("wss://karura.api.onfinality.io/public-ws");
     }
 
+    /**
+     * Retrieves account data for the Karura account address of the class
+     * 
+     * @returns {Promise<FrameSystemAccountInfo>}
+     */
     async accountData() {
         const provider = new WsProvider("wss://karura.api.onfinality.io/public-ws");
         const api = new ApiPromise(options({ provider }));
@@ -24,13 +37,19 @@ class KaruraClient {
         return accountData.toHuman();
     }
 
-    async getFees(recipient: string, sender: string) {
+    /**
+     * Gets potential fees for a transaction on Karura
+     * 
+     * @param {string}      recipient   transaction receiver address
+     * @returns {Promise<RuntimeDispatchInfo>}
+     */
+    async getFees(recipient: string) {
         const api = new ApiPromise(options({ provider: this.provider }));
         await api.isReadyOrError;
 
         const info = await api.tx.balances
             .transfer(recipient, 123)
-            .paymentInfo(sender);
+            .paymentInfo(this.address);
 
         const [amount, unit] = info.partialFee.toHuman().split(" ");
         return `${this.karUnitsToKAR(amount, unit)} KAR`;
@@ -47,6 +66,26 @@ class KaruraClient {
             return amount;
         }
     }    
+
+    /**
+     * Transfers a token from the class address to a recipient Karura account address
+     * 
+     * @param {string}      recipient   transfer receiver address 
+     * @param {string}      token   id of token to transfer - Examples "KSM", "KAR" 
+     * @param {string}      amount  amount of tokens to transfer 
+     */
+    async transfer(recipient: string, token: string, amount: string) {
+        const api = new ApiPromise(options({ provider: this.provider }));
+        await api.isReadyOrError;
+
+        this.accountData().then((data) => this.logger.log("Before transfer account data", data));
+
+        const hash = await api.tx.currencies
+            .transfer(recipient, { Token: token,}, amount)
+            .signAndSend(this.address);
+
+        this.logger.log("Transfer sent with hash", hash.toHex());
+    }
 }
 
 module.exports = KaruraClient;
