02ada680491211ebe128039e3697b9370a277098
Lester Houtenbos <ljphoutenbos@gmail.com>
2021-11-03 21:55:09 +0100

    Update imports and build files.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const KrakenClient = require("./connectors/kraken/kraken-client");
 const KaruraClient = require("./build/connectors/karura/karura-client");
-const Balance = require("./lib/balance");
-const { askPassword, getOrSetApi } = require("./build/lib/get-credentials");
-const config = require("./config/trading-config");
+const Balance = require("./src/lib/balance");
+const { askPassword, getOrSetApi } = require("./build/src/lib/get-credentials");
+const config = require("./src/config/trading-config");
 
 const clients = new Map();
 
