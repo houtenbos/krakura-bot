8e6ab90df628a23cd52ed6f81a2cbf1af50336f3
Matthew Ashley <matthewashley1@gmail.com>
2021-11-03 11:25:21 -0400

    Removed console logs from index.js

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ const clients = new Map();
 	let balance = new Balance([...clients.entries()], config.currencies);
 	await balance.isReady;
 
-	console.log("Credentials configured");
-	console.log("Looking for profitable trades");
-
 	// check buy kraken sell karura opportunity
 	while( true ){
 		for( const [buyPlatform, sellPlatform] of config.crossPlatforms){
